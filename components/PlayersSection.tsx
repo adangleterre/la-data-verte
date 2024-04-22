fca29aad8b133ca83ba6de6560c671ebb72fdfda
AntoineD <dangleterre.antoine@gmail.com>
2024-04-22 19:24:13 +0200

    Add player by position in player section

--- components/PlayersSection.tsx
+++ components/PlayersSection.tsx
@@ -2,21 +2,30 @@ import PlayerCard from "./PlayerCard"
 
 async function PlayersSection() {
     return (
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-16">
             <div>
-                <p className="font-bold text-2xl mb-3">Gardiens</p>
+                <p className="font-bold text-3xl mb-6">Gardiens</p>
                 <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-                    <PlayerCard />
+                    <PlayerCard positionCategory="Goalkeeper"/>
                 </div>
             </div>
             <div>
-                <p className="font-bold text-2xl">Défenseurs</p>
+                <p className="font-bold text-3xl mb-6">Défenseurs</p>
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+                    <PlayerCard positionCategory="Defender"/>
+                </div>
             </div>
             <div>
-                <p className="font-bold text-2xl">Milieux</p>
+                <p className="font-bold text-3xl mb-6">Milieux</p>
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+                    <PlayerCard positionCategory="Midfielder"/>
+                </div>
             </div>
             <div>
-                <p className="font-bold text-2xl">Attaquants</p>
+                <p className="font-bold text-3xl mb-6">Attaquants</p>
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+                    <PlayerCard positionCategory="Attacker"/>
+                </div>
             </div>
         </div>
     )
