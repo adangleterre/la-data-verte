3b1c8377ba94b586031e23e0fdcd3f7ad63499dc
AntoineD <dangleterre.antoine@gmail.com>
2024-04-25 14:00:42 +0200

    Fix spacing

--- components/StatsSection.tsx
+++ components/StatsSection.tsx
@@ -27,9 +27,9 @@ async function StatsSection() {
         <>
             <h2 className="text-center text-green-950 font-bold text-4xl mb-4">En bref</h2>
             {filteredTeam.map((team) => (
-                <div key={team.teamId} className="text-3xl mx-auto max-w-4xl text-center mb-6">Sainté est {team.rank}eme de la ligue 2 BKT avec {team.points} points. L&apos;effectif est composé de 42 joueurs d&apos;une moyenne d&apos;age de 26 ans.</div>
+                <div key={team.teamId} className="text-xl md:text-3xl mx-auto max-w-4xl text-center mb-6">Sainté est {team.rank}eme de la ligue 2 BKT avec {team.points} points. L&apos;effectif est composé de 42 joueurs d&apos;une moyenne d&apos;age de 26 ans.</div>
             ))}
-            <div className="flex justify-center gap-8">
+            <div className="flex flex-col items-center sm:flex-row justify-center gap-4 sm:gap-8">
                 <Link href="/standing" className="flex items-center gap-1">
                     Consulter le classement <GoArrowUpRight />
                 </Link>
